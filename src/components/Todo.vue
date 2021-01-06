4c9e9fc750f4b82b06bce00b02d0919151bfead6
chaensday <sdl.cypark@gmail.com>
2021-01-06 11:23:40 +0900

    Todo 컴포넌트에 기본 뼈대 생성

---
+++ src/components/Todo.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <div>
+        <input type="checkbox">
+        <span class="ml-3">Buy a car</span>
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
