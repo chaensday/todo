929253cdfef806494771db86c545002e95f9a12a
chaensday <sdl.cypark@gmail.com>
2021-01-06 11:25:28 +0900

    App.vue에 Todo 컴포넌트import함

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,19 @@
     <h1>Todo App</h1>
     <input type="text" class="w-100 p-2" placeholder="Type todo">
     <hr>
-
-    <div>
-      <input type="checkbox">
-      <span class="ml-3">Buy a car</span>
-    </div>
+    <Todo />
 
   </div>
 </template>
 
 <script>
+import Todo from '@/components/Todo.vue';
+
+export default {
+  components: {
+    Todo,
+  }
+}
 
 </script>
 
