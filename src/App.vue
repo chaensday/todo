1036ff592184f493e91a04cbba81d850b96024dc
cypark <cypark@bagchaeyeon-ui-MacBookPro.local>
2021-01-06 11:03:58 +0900

    App.vue에 기본뼈대 추가

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
+    <h1>Todo App</h1>
+    <input type="text" class="w-100 p-2" placeholder="Type todo">
+    <hr>
+
+    <div>
+      <input type="checkbox">
+      <span class="ml-3">Buy a car</span>
+    </div>
+
   </div>
 </template>
 
