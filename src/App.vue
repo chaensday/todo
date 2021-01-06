1b58803ae17e561db3a01b75c0a0673dff98c348
chaensday <sdl.cypark@gmail.com>
2021-01-06 11:44:20 +0900

    input & todo list 추가 기능 완성

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app" class="container">
     <h1>Todo App</h1>
     <input 
+      v-model="todoText"
       type="text" 
       class="w-100 p-2" 
       placeholder="Type todo"
@@ -26,6 +27,7 @@ export default {
   },
   data(){
     return {
+      todoText: '',
       todos: [
         { id: 1, text: 'buy a car', checked: false},
         { id: 2, text: 'play game', checked: false},
@@ -36,6 +38,13 @@ export default {
     addToDo(e){
       console.log(e);
       console.log(e.target.value);
+
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false,
+      });
+      this,this.todoText='';
     }
   }
 }
